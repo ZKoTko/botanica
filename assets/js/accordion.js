cc81ebffda8de006370a84cc3752d766bb61ed93
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2024-11-17 18:31:25 +0400

    ftghf

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,24 +1,21 @@
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-    accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", event => {
-        
-        // Разворачивается только один (при разворачивании второго первый сворачивается)
-        
-        // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-        // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-        //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-        //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-        // }
+accordionItemHeaders.forEach(accordionItemHeader => {
+    // Раскрываем контент, если есть класс "active"
+    if (accordionItemHeader.classList.contains("active")) {
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
 
+    accordionItemHeader.addEventListener("click", event => {
+
+        // Переключаем текущий аккордион
         accordionItemHeader.classList.toggle("active");
         const accordionItemBody = accordionItemHeader.nextElementSibling;
-        if(accordionItemHeader.classList.contains("active")) {
-        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
         }
-        else {
-        accordionItemBody.style.maxHeight = 0;
-        }
-        
     });
 });
